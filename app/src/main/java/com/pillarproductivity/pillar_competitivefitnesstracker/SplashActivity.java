248aabf4ad2e5cfda4a3307b3e7175fbe9530109
Tyler Condie <tylerwcondie@gmail.com>
2019-04-11 19:50:44 -0600

    Began adding UI elements

---
+++ app/src/main/java/com/pillarproductivity/pillar_competitivefitnesstracker/SplashActivity.java
@@ -0,0 +1,11 @@
+package com.pillarproductivity.pillar_competitivefitnesstracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
