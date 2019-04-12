d725941addda5080df6bb0af8abfe8120b80ef6f
Tyler Condie <tylerwcondie@gmail.com>
2019-04-12 01:55:11 -0600

    Created authentication verification on splash screen to determine next activity.

--- app/src/main/java/com/pillarproductivity/pillar_competitivefitnesstracker/SplashActivity.java
+++ app/src/main/java/com/pillarproductivity/pillar_competitivefitnesstracker/SplashActivity.java
@@ -4,12 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent signIntent = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(signIntent);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent successIntent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(successIntent);
+        }
+        Intent failIntent = new Intent(SplashActivity.this, SignInActivity.class);
+        startActivity(failIntent);
     }
 }
 
