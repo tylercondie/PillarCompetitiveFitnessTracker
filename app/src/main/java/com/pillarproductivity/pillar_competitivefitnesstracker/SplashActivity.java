3efc0beb2ebd6048f216154ee26f4d1d8052f1a5
Tyler Condie <tylerwcondie@gmail.com>
2019-04-12 00:49:05 -0600

    Added splash image and linked to firebase

--- app/src/main/java/com/pillarproductivity/pillar_competitivefitnesstracker/SplashActivity.java
+++ app/src/main/java/com/pillarproductivity/pillar_competitivefitnesstracker/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.pillarproductivity.pillar_competitivefitnesstracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -7,5 +8,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent signIntent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(signIntent);
     }
 }
+
